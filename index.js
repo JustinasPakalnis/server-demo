7f50f40fb4ff263ebcdeadabbd84de8c2efef019
JustinasPakalnis <justinas.p@inbox.lt>
2024-09-19 13:11:01 +0300

    up

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ const app = express();
 const port = 5114;
 
 app.get("/", (req, res) => {
-  return res.send("Home page");
+  return res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    laba diena cia yr main
+</body>
+</html>`);
 });
 
 app.get("/services", (req, res) => {
-  return res.send("Services page: html, css, js, git");
+  return res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SERVICES PAGE</h1>
+    <nav>
+<a href="services/html">HTML</a>
+<a href="services/css">CSS</a>
+<a href="services/js">JS</a>
+<a href="services/git">GIT</a>
+    </nav>
+</body>
+</html>`);
 });
 
 app.get("/services/:name", (req, res) => {
